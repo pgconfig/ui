4f5819dd189fc0263d4bc8dceb7fb0fd91985e1c
Sebastian Webber <sebastian@swebber.me>
2025-12-28 04:56:32 -0300

    feat(Form): update PostgreSQL version options to include 18 as latest

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -108,7 +108,9 @@
                 v-model="form.pg_version"
                 expanded
               >
-                <option value="16">16 (Latest)</option>
+                <option value="18">18 (Latest)</option>
+                <option value="17">16</option>
+                <option value="16">16</option>
                 <option value="15">15</option>
                 <option value="14">14</option>
                 <option value="13">13</option>
@@ -139,7 +141,7 @@ export default {
     return {
       form: {
         max_connections: 100,
-        pg_version: 16,
+        pg_version: 18,
         environment_name: "WEB",
         total_ram: 4,
         cpus: 2,
